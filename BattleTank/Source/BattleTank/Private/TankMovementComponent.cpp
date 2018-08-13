5030b40bb58eb5f637e8767121201c7183bb45cb
Christopher Johnston <chris@Tattersail.local>
2018-08-13 18:30:20 +0100

    clamp throttle; add copyright notice to project settings; change visibility of RequestDirectMove

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,4 +1,4 @@
-// Fill o§t your copyright notice in the Description page of Project Settings.
+// Copyright CPJ 2018
 
 #include "TankMovementComponent.h"
 #include "TankTrack.h"
@@ -12,17 +12,17 @@ void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
     if (!LeftTrack || !RightTrack) { return; }
-    LeftTrack->SetThrottle(Throw);
-    RightTrack->SetThrottle(Throw);
-    // TODO: Prevent double speed due to dual control use.
+    auto ClampedThrow = FMath::Clamp<float>(Throw, -1, 1);
+    LeftTrack->SetThrottle(ClampedThrow);
+    RightTrack->SetThrottle(ClampedThrow);
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
 {
     if (!LeftTrack || !RightTrack) { return; }
-    LeftTrack->SetThrottle(Throw);
-    RightTrack->SetThrottle(-Throw);
-    // TODO: Prevent double speed due to dual control use.
+    auto ClampedThrow = FMath::Clamp<float>(Throw, -1, 1);
+    LeftTrack->SetThrottle(ClampedThrow);
+    RightTrack->SetThrottle(-ClampedThrow);
 }
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
@@ -30,14 +30,11 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
     auto AIForwardIntention = MoveVelocity.GetSafeNormal();
     auto TankForward = GetOwner()->GetActorForwardVector();
 
-
     auto ForwardThrow = FVector::DotProduct(AIForwardIntention, TankForward);
     IntendMoveForward(ForwardThrow);
 
     auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
     IntendTurnRight(RightThrow);
-    // auto TankName = GetOwner()->GetName();
-	// UE_LOG(LogTemp, Warning, TEXT("%s MoveVelocity %s"), *TankName, *MoveVelocityString)
 }
 
 
