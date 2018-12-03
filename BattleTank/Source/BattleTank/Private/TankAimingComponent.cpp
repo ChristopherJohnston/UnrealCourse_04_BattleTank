9400e6eed99ab5444007d3f5839739d1bb6c31cb
Chris Johnston <christopher_johnston@me.com>
2018-12-03 23:14:06 +0000

    Refactored aiming code (AimAt method) from Tank into TankAimingComponent, removed dependencies on tank in TankAimingComponent and TankAiController (#2)

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -19,7 +19,7 @@ void UTankAimingComponent::Initialise(UTankBarrel* BarrelReference, UTankTurret*
 	Turret = TurretReference;
 }
 
-void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed) {
+void UTankAimingComponent::AimAt(FVector HitLocation) {
 	if (!ensure(Barrel)) { return; }
 	
 	FVector OutLaunchVelocity;
--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -1,9 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "Tank.h"
-#include "TankBarrel.h"
 #include "Projectile.h"
-#include "TankAimingComponent.h"
-
+#include "TankBarrel.h"
 
 // Sets default values
 ATank::ATank()
@@ -14,14 +12,6 @@ ATank::ATank()
 
 void ATank::BeginPlay() {
 	Super::BeginPlay();
-	TankAimingComponent = FindComponentByClass<UTankAimingComponent>();
-}
-
-void ATank::AimAt(FVector HitLocation)
-{
-	if (!ensure(TankAimingComponent)) { return; }
-	
-	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 }
 
 void ATank::Fire()
