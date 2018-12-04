a1b5d438497d8da71261c68d3318e62e67da1247
Chris Johnston <christopher_johnston@me.com>
2018-12-04 00:17:17 +0000

    check is reloaded on tick using EFiringState (#4)

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -34,12 +34,16 @@ public:
 
 protected:
 	UPROPERTY(BlueprintReadOnly, Category="State")
-	EFiringState FiringState = EFiringState::Locked;
+	EFiringState FiringState = EFiringState::Reloading;
 
 private:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	virtual void BeginPlay() override;
+	
 	UPROPERTY(EditDefaultsOnly, Category="Firing")
 	bool FiringEnabled = true;
 
