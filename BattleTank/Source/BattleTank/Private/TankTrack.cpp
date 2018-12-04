e4e5b98b4a585b4de2276ee871b4a3b17704fbe9
Chris Johnston <christopher_johnston@me.com>
2018-12-04 22:14:38 +0000

    Videos/188 - Sideways force correction (#6)
    * check that a pawn exists when aiming to prevent crash if not possessing
    
    * Fix tank aiming component losing projectile reference
    
    * added sideways force correction to tracks to prevent "slipping"

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,6 +2,22 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack() {
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+
+	// Required acceleration this frame that needs to be corrected.
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
+	TankRoot->AddForce(CorrectionForce);
+
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	auto Name = GetName();
