59e30ac6c36abb314b1318b05a01f9d0ff80ef19
Chris Johnston <christopher_johnston@me.com>
2019-01-15 23:23:00 +0000

    tank health bar

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -12,6 +12,11 @@ class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
+public:
+	// Returns current health as a percentage of starting health, between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 private:
 	ATank();
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
